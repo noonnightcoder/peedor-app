473abe6a4e77486f36f842b246b9579d5caa0699
Lux <soklux@gmail.com>
2018-06-05 15:04:58 +0700

    Add dashboard permission

--- protected/views/site/pages/_customer.php
+++ protected/views/site/pages/_customer.php
@@ -1,6 +1,7 @@
 <div class="col-xs-6">
     <h3 class="header smaller lighter green">👨‍👩‍👦‍👦ថែរក្សាអតិថិជនរបស់អ្នក TAKING CARE YOU CUSTOMER</h3>
-    <?php if (ckacc('report.sale.analytic') || ckacc('stock.out') ) { ?>
+
+    <?php if (ckacc('report.sale.analytic') ) { ?>
         <?php echo TbHtml::linkButton('CRM', array(
             //'color' => TbHtml::BUTTON_COLOR_LINK,
             'class' => 'btn btn-app btn-pink',
@@ -8,13 +9,23 @@
             'url' => $this->createUrl('report/saleWeeklyByCustomer'),
             'title' => t('Customer Relationship Management','app')
         )); ?>
+    <?php } else { ?>
+        <?php echo TbHtml::linkButton('CRM', array(
+            'color' => TbHtml::BUTTON_COLOR_DEFAULT,
+            'class' => 'btn btn-app btn-pink',
+            'icon' => 'ace-icon fa fa-ban ',//'ace-icon fa fa-handshake-o' . ' bigger-200',
+            'url' => $this->createUrl('report/saleWeeklyByCustomer'),
+            'title' => t('Customer Relationship Management','app'),
+            'disabled' => true,
+        )); ?>
     <?php } ?>
+
 </div>
 
 <div class="col-xs-6">
     <h3 class="header smaller lighter green">🔮របាយការណ៏ថ្លាដូចកញ្ចក់ Crystal Clear + 360º Reports</h3>
 
-    <?php if (ckacc('report.sale.analytic') || ckacc('stock.out') ) { ?>
+    <?php if (ckacc('report.sale.analytic') ) { ?>
         <?php echo TbHtml::linkButton('Report', array(
             //'color' => TbHtml::BUTTON_COLOR_LINK,
             'class' => 'btn btn-app btn-pink',
@@ -23,4 +34,5 @@
             'title' => t('Crystal Clear 360º Reports','app')
         )); ?>
     <?php } ?>
+
 </div>
--- protected/views/site/pages/_sale.php
+++ protected/views/site/pages/_sale.php
@@ -1,7 +1,7 @@
 <div class="col-xs-6">
     <h3 class="header smaller lighter green">🚀បង្កើនការលក់របស់អ្នក BOOST YOUR SALES</h3>
 
-    <?php if (ckacc('sale.create') || ckacc('sale.update') ) { ?>
+    <?php if (ckacc('sale.read') || ckacc('sale.create') || ckacc('sale.update') ) { ?>
 
         <?php echo TbHtml::linkButton(sysMenuSaleOrder(), array(
             'color' => TbHtml::BUTTON_COLOR_PRIMARY,
@@ -11,9 +11,18 @@
             'title' => sysMenuSaleOrder() . ' Add',
         )); ?>
 
+    <?php } else { ?>
+        <?php echo TbHtml::linkButton(sysMenuSaleOrder(), array(
+            'color' => TbHtml::BUTTON_COLOR_PRIMARY,
+            'class' => 'btn btn-app btn-sm',
+            'icon' => 'ace-icon fa fa-ban',//'ace-icon '. sysMenuSaleOrderIcon() . '  icon-animated-vertical ' . ' bigger-200',
+            'url' => $this->createUrl('saleItem/create'),
+            'title' => sysMenuSaleOrder() . ' Add',
+            'disabled' => true,
+        )); ?>
     <?php } ?>
 
-    <?php if (ckacc('sale.review') ) { ?>
+    <?php if (ckacc('sale.validate')) { ?>
 
         <?php echo TbHtml::linkButton('Order 2 ✅', array(
             'color' => TbHtml::BUTTON_COLOR_PRIMARY,
@@ -27,6 +36,21 @@
             'title' => sysMenuSaleOrderToValidate()
         )); ?>
 
+    <?php } else { ?>
+
+        <?php echo TbHtml::linkButton('Order 2 ✅', array(
+            'color' => TbHtml::BUTTON_COLOR_PRIMARY,
+            'class' => 'btn btn-app btn-sm',
+            'icon' => 'ace-icon fa fa-ban',
+            'url' => $this->createUrl('saleItem/create',array (
+                'status' => param('sale_submit_status'),
+                'user_id' => getEmployeeId(),
+                'title' => sysMenuSaleOrderToValidate(),
+            )),
+            'title' => sysMenuSaleOrderToValidate(),
+            'disabled' => true,
+        )); ?>
+
     <?php } ?>
 </div>
 
@@ -34,7 +58,7 @@
 
     <h3 class="header smaller lighter green">💵គ្រប់គ្រងវិក័យប័ត្ររបស់អ្នក MANAGE YOUR INVOICE</h3>
 
-    <?php if (ckacc('sale.create') || ckacc('sale.update') ) { ?>
+    <?php if (ckacc('sale.create') || ckacc('sale.update') || ckacc('sale.delete') ) { ?>
 
         <?php echo TbHtml::linkButton(sysMenuInvoice(), array(
             'color' => TbHtml::BUTTON_COLOR_SUCCESS,
@@ -43,14 +67,33 @@
             'url' => $this->createUrl('saleItem/create'),
         )); ?>
 
+    <?php } else { ?>
+
+        <?php echo TbHtml::linkButton(sysMenuInvoice(), array(
+            'color' => TbHtml::BUTTON_COLOR_SUCCESS,
+            'class' => 'btn btn-app btn-sm',
+            'icon' => 'ace-icon fa fa-ban',//'ace-icon '. sysMenuInvoiceIcon() . ' icon-animated-vertical ' . ' bigger-200',
+            'url' => $this->createUrl('saleItem/create'),
+            'disabled' => true,
+        )); ?>
+
     <?php } ?>
 
-    <?php if (ckacc('report.accounting')) { ?>
+    <?php if (ckacc('report.account')) { ?>
         <?php echo TbHtml::linkButton('Revenue', array(
             'color' => TbHtml::BUTTON_COLOR_SUCCESS,
             'class' => 'btn btn-app btn-sm',
             'icon' => 'ace-icon '. sysMenuInvoiceIcon() . ' bigger-200',
             'url' => $this->createUrl('report/profitDailySum'),
         )); ?>
+    <?php } else { ?>
+        <?php echo TbHtml::linkButton('Revenue', array(
+            'color' => TbHtml::BUTTON_COLOR_SUCCESS,
+            'class' => 'btn btn-app btn-sm',
+            'icon' => 'ace-icon fa fa-ban',//'ace-icon '. sysMenuInvoiceIcon() . ' bigger-200',
+            'url' => $this->createUrl('report/profitDailySum'),
+            'disabled' => true,
+        )); ?>
     <?php } ?>
+
 </div>
